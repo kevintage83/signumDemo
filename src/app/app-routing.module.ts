4a6bfe00fb076d31dd60c3171f2951b891d1219a
Kevin Burns <kevinwburns@gmail.com>
2019-11-13 17:03:28 -0500

    fixed page routing, added login pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,23 @@
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { LoginComponent } from './login/login.component';
+import { AppComponent } from './app.component';
+import { JasonPressBioComponent } from './jason-press-bio/jason-press-bio.component';
+import { HomeComponent } from './home/home.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+
+const routes: Routes = [
+    { path: '', pathMatch: 'full', component: HomeComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'jbio', component: JasonPressBioComponent },
+    { path: 'resetPassword', component: ForgotPasswordComponent },
+    { path: '**', redirectTo: '' }
+];
 
 @NgModule({ 
-  
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [LoginComponent, JasonPressBioComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RouterModule, Routes } from '@angular/router';
-import { CharlesMyersBioComponent } from './charles-myers-bio/charles-myers-bio.component';
-import { AndrewBishopBioComponent } from './andrew-bishop-bio/andrew-bishop-bio.component';
-import { AngelaDaltonBioComponent } from './angela-dalton-bio/angela-dalton-bio.component';
-import { JasonPressBioComponent } from './jason-press-bio/jason-press-bio.component';
-import { AnnaRosenbergBioComponent } from './anna-rosenberg-bio/anna-rosenberg-bio.component';
+import { HomeComponent } from './home/home.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
-const appRoutes: Routes = [
-  { path: 'charlesMyersBio', component: CharlesMyersBioComponent },
-  { path: 'angelaDaltonBio', component: AngelaDaltonBioComponent },
-  { path: 'annaRosenbergBio', component: AnnaRosenbergBioComponent },
-  { path: 'andrewBishopBio', component: AndrewBishopBioComponent },
-  { path: 'jasonPressBio', component: JasonPressBioComponent }
-]
 
 @NgModule({
   declarations: [
     AppComponent,
-    CharlesMyersBioComponent,
-    AndrewBishopBioComponent,
-    AngelaDaltonBioComponent,
-    JasonPressBioComponent,
-    AnnaRosenbergBioComponent
+    routingComponents,
+    HomeComponent,
+    ForgotPasswordComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(
-      appRoutes,
-      { enableTracing: true } // <-- debugging purposes only
-    )
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
