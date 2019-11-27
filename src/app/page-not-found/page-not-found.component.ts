fefc1bb4c0af03b8fcd842380180a206009c1422
Kevin Burns <kevinwburns@gmail.com>
2019-11-27 12:35:22 -0500

    added more mobile flex content to pgNotFound

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageNotFoundComponent implements OnInit {
 
+  currentYear: number = new Date().getFullYear();
+
   constructor() { }
 
   ngOnInit() {
