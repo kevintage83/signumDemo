73eace160476325f46a3610e20ceafaaeb629e18
Kevin Burns <kevinwburns@gmail.com>
2019-11-27 14:52:31 -0500

    added final mobile flex content to homePage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  currentYear: number = new Date().getFullYear();
+
+
   constructor() { }
 
   ngOnInit() {
