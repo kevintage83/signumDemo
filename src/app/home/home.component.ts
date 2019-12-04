d0d517a998775a83a2ad707d98d94cbef01c9b16
Kevin Burns <kevinwburns@gmail.com>
2019-12-04 12:38:44 -0500

    added hr, color changes, font changes to bio sections

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,31 +9,33 @@ export class HomeComponent implements OnInit {
 
   currentYear: number = new Date().getFullYear();
 
-
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
   sendCharlesMessage(){
     alert("Charles Myers was most recently Vice Chairman of Evercore ISI and a member of Evercore ISI's Executive Committee and Investment Policy Committee. He was the founding partner of Evercore's Institutional Equities business in 2009. \n\nPrior to Evercore, Charles was a Board Member and Managing Director of Fox-Pitt Kelton, which he joined in 2004. He held a number of senior positions during his tenure, and as Global Head of Equities, was responsible for research, sales and trading of all equities products across the United States, Europe and Asia. Charles has over 25 years experience in global capital markets, having worked previously at UBS. \n\n Charles serves on the Board of Trustees of the Central Park Conservancy, the Board of the Mayor's Fund for NYC, and previously served on the NYC Host Committee for the 2016 Democratic Convention. He also has over 20 years experience in US electoral politics, having advised numerous presidential, congressional, gubernatorial and mayoral campaigns. He served as a senior member of the Finance Committees of the 2008 and 2012 Obama Presidential campaigns as well as the 2016 Hillary Clinton for President campaign. \n\nAn American citizen, Charles grew up in Libya, Peru and Indonesia, has a B.A. from Amherst College in Massachusetts and a post-graduate M. Phil degree from Cambridge University in England. He lives in New York City with his four children.");
   };
 
   sendAngelasMessage(){
-    alert("Angela Dalton is a Senior Partner of Signum Global Advisors and CEO of Signum Innovation Capital. She has 19 years of experience in capital markets and long relationships with institutional investors and publicly traded companies in the Tech Media and Telecom sectors. \n\nAfter working for nine years at UBS and at an Equity Hedge Fund as Head of Marketing and Broker Relations, she worked for two M&A firms, Evercore and Guggenheim. At Evercore, she participated as an initial investor and a Co-Founder of the Equities business in 2010 and helped the team build the business to 87 people in Research, Sales and Trading. \n\nMost recently at Guggenheim, Angie was the Sector Head of TMT in Equities and analyzed technologies such as AI, digital media, data centers, eSports, blockchain and cryptocurrency for institutional investors. /n/nAt Signum, she spends most of her time at the intersection of Policy and Emerging Tech, with an emphasis on blockchain and cryptocurrency. Angie is on the Advisory Board of the Wall Street Blockchain Alliance and holds a BS and a BA from The University of Kansas and an MBA from The University of Chicago.");
+    alert("Angela Dalton is a Senior Partner of Signum Global Advisors and CEO of Signum Innovation Capital. She has 19 years of experience in capital markets and long relationships with institutional investors and publicly traded companies in the Tech Media and Telecom sectors. \n\nAfter working for nine years at UBS and at an Equity Hedge Fund as Head of Marketing and Broker Relations, she worked for two M&A firms, Evercore and Guggenheim. At Evercore, she participated as an initial investor and a Co-Founder of the Equities business in 2010 and helped the team build the business to 87 people in Research, Sales and Trading. \n\nMost recently at Guggenheim, Angie was the Sector Head of TMT in Equities and analyzed technologies such as AI, digital media, data centers, eSports, blockchain and cryptocurrency for institutional investors. \n\nAt Signum, she spends most of her time at the intersection of Policy and Emerging Tech, with an emphasis on blockchain and cryptocurrency. Angie is on the Advisory Board of the Wall Street Blockchain Alliance and holds a BS and a BA from The University of Kansas and an MBA from The University of Chicago.");
   };
 
   sendAndrewsMessage(){
     alert("Andrew Bishop, Partner at Signum Global Advisors, is an established political analyst with a ten-year track record of advising institutional investors, corporates and governments on political risks in developed, emerging, and frontier markets. As Global Head of Policy Research, Andrew directs and oversees Signum Global’s policy-related analytical products and partners with clients to address questions about critical political uncertainties. Andrew is head of Signum's Washington, D.C. office. \n\nAndrew joined Signum from Eurasia Group where he served as Deputy Head of Research and was also a Director in the firm’s Global Macro practice. Prior to that, Andrew was a senior manager in the World Economic Forum’s strategic foresight and risk analysis unit for six years, during which he regularly participated in the organization’s annual meeting in Davos. \n\nAndrew is a master’s graduate of the London School of Economics and ‘SciencesPo. Paris’ and was a fellow at the Johns Hopkins University in Washington, D.C. He is Term Member of the Council on Foreign Relations and actively participates in the Munich Security Conference’s leadership program.");
-  }
+  };
 
   sendAnnasMessage(){
     alert("Anna Rosenberg is Partner, Head of Europe and UK and member of Signum’s Global Management Committee. \n\nOver the past ten years, Anna has advised executives of multinational companies in managing their global market portfolios. She helps senior executives better plan for and respond to forces outside of their control, such as political and economic risk, to ensure their businesses can navigate through difficult times and continue to perform. \n\nBefore joining Signum, Anna worked as Director of Global Management Insights at Frontier Strategy Group and also headed the firms' Sub-Saharan Africa research practice. Previously, Anna worked as Head of Research for IC Publications, where she was in charge of putting together major investment conferences with a focus on emerging markets. \n\nHer work on business strategy has been featured in Harvard Business Review, the Wall Street Journal, the BBC and CNBC, among others. Anna is also a Visiting Scholar at the London Institute of Banking and Finance and a BBC Expert Woman. Anna holds an MA with Distinction from the SOAS, University of London. She speaks fluently German, Spanish, and Portuguese, reads French and Italian and basic Arabic.");
-  }
+  };
 
   sendJasonsMessage(){
     alert("Jason Press, Senior Partner, leads Signum Global Advisors's sales and distribution efforts. He works with a wide variety of financial institutions providing insights and forecasting how political and global macro developments move equity, foreign exchange, and fixed income markets - with more than 20 years in the financial markets industry. \n\nJason has been a senior member of Eurasia Group and previous to that worked in emerging markets investment strategy at Citigroup and Goldman Sachs in New York, Hong Kong, Singapore, and Sao Paulo. \n\nJason holds a master's degree in international finance and banking from Columbia University's School of International and Public Affairs. He regularly presents to a wide range of audiences on geopolitical and macro trends and their implications for financial markets.");
-  }
+  };
+
+  sendSignUpMessage() {
+    alert("Thank you! One of our experts will contact you shortly!");
+  };
 
 }
  
